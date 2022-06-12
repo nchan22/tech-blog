f5e03937e78c57fb64589118a66da6b835b0ab3f
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:54:30 -0400

    Add get all users post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,3 +2,32 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
+
+// get all users
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "title", "created_at", "post_content"],
+    order: [["created_at", "DESC"]],
+    include: [
+      // Comment model here -- attached username to comment
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "twitter", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "twitter", "github"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
