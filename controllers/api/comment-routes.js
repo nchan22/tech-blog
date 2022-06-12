9834b21e609c6cf572e7cd6c0d944d87d4c3e9fb
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:30:08 -0400

    Add delete a comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -27,3 +27,24 @@ router.post("/", withAuth, (req, res) => {
       });
   }
 });
+
+router.delete("/:id", withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
