47aade65ea51cea126fe5044674de88931776130
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:34:47 -0400

    Add find individual user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,3 +14,38 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// GET /api/users/1
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_content", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
