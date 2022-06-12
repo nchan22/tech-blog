4d93e404615368a5b1328392e62162238fee70c6
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:36:51 -0400

    Add delete user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -134,3 +134,25 @@ router.put("/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// DELETE /api/users/1
+router.delete("/:id", withAuth, (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
