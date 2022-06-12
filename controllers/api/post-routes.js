4efaec07af38453b3bdfd1c95e12fb40df84e02d
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:55:51 -0400

    Add update user post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -81,3 +81,29 @@ router.post("/", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// update user post
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      post_content: req.body.post_content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
