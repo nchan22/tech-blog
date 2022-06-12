a1f0698f37345e52bc1f7a422f6a92580a4e146c
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-12 00:55:26 -0400

    Add create user post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -67,3 +67,17 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// create user post
+router.post("/", withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_content: req.body.post_content,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
