5658a8137ca664f49b5cdd740823f665dc420cd8
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-14 01:22:28 -0400

    Update to check for password

--- models/User.js
+++ models/User.js
@@ -3,4 +3,9 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 // create our User model that extends from the Model class
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
