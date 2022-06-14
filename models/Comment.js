f541fe308760db1835114d1e11e34df4c01bdf53
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-14 01:20:09 -0400

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -3,3 +3,46 @@ const sequelize = require("../config/connection");
 
 // create comment class that has properties of Model
 class Comment extends Model {}
+
+// create comment IDs and text
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
