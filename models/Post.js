bfc64880204ddee4302a7fd3adb37b4ca3964080
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-14 01:21:05 -0400

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,3 +3,38 @@ const sequelize = require("../config/connection");
 
 // create our Post model that extends from Model class
 class Post extends Model {}
+
+// create fields & columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
