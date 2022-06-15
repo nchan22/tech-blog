0181d95508e3c4a609ea19c10e928714a3407885
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-15 00:51:57 -0400

    update variable paths

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 
-const helpers = require("utils/helpers.js");
+const helpers = require("./utils/helpers");
 
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
