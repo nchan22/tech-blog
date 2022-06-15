6cfbdb9ae456c6edb73e21b501653a537d0e826e
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-15 01:00:24 -0400

    update path

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
-const sequelize = require("config/connection.js");
-const withAuth = require("utils/auth.js");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
 
 // get all users
 router.get("/", (req, res) => {
