c910e51e5f262762ca5ece7beff0a834e2280799
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-06-15 00:42:34 -0400

    update files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = require("./controllers");
-const sequelize = require("config/connection.js");
+const sequelize = require("./config/connection");
 const path = require("path");
 
 const helpers = require("utils/helpers.js");
